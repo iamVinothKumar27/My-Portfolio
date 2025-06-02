f2d08a75885862adc394b7e2d60ee1be3c3873f9
VINOTH KUMAR T S <t.s.vinothtce@gmail.com>
2025-06-02 23:58:18 +0530

    Fixed Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,11 @@
   const headerToggleBtn = document.querySelector('.header-toggle');
 
   function headerToggle() {
-    document.querySelector('#header').classList.toggle('header-show');
+    document.querySelector('#navmenu').classList.toggle('navmenu-active');
     headerToggleBtn.classList.toggle('bi-list');
     headerToggleBtn.classList.toggle('bi-x');
   }
+  
   headerToggleBtn.addEventListener('click', headerToggle);
 
   /**
